e30a4eeb108dca67a606cb3dd4cf334f786da092
dcharoulis <dimitrios.charoulis@lunatech.com>
2015-10-14 17:18:46 +0200

    Fixes #14 Send response to HipChat with results from scala compiler

--- src/main/scala/com/lunatech/lunabot/LunabotServlet.scala
+++ src/main/scala/com/lunatech/lunabot/LunabotServlet.scala
@@ -3,10 +3,21 @@ package com.lunatech.lunabot
 import com.lunatech.lunabot.model._
 import org.scalatra._
 import org.json4s.{DefaultFormats, Formats}
-import org.json4s.JsonDSL._
-import org.scalatra.json._
 import org.slf4j.LoggerFactory
+import org.scalatra.json._
+import scala.sys.process._
+import dispatch._, Defaults._
+import scala.util.{Failure, Success}
 
+
+/**
+ * Application to handle POST requests received from HipChat.
+ *
+ * A HipChat request contains information in JSON format which includes
+ * a piece of code to be executed in scala REPL in asynchronous mode.
+ * Once the response is sent by REPL, an POST request including the
+ * scala result is sent to HipChat.
+ */
 class LunabotServlet extends ScalatraServlet with JacksonJsonSupport {
 
   val logger = LoggerFactory.getLogger(getClass)
@@ -25,10 +36,39 @@ class LunabotServlet extends ScalatraServlet with JacksonJsonSupport {
     logger.debug(request.body)
     val jsonValue = parse(request.body.replace("mention_name", "mentionName"))
     val hipchatMsg = jsonValue.extract[HipChatMessage]
-    val replResponse = "Response from REPL"
-    ("color" -> "red") ~
-      ("message" -> replResponse) ~
-      ("notify" -> false) ~
-      ("message_format" -> "text")
+    val authToken = System.getenv("AUTH_TOKEN")
+    val urlStr = "https://api.hipchat.com/v2/room/roomId/notification?auth_token="
+
+    val executionFuture: Future[String] = Future {
+      val cmd1: String = hipchatMsg.item.message.message.replace("/scala", "")
+      val output: Seq[String] = Seq("scala", "-e", cmd1)
+      output.!!
+    }
+
+    executionFuture.onComplete {
+      case Success(result) => {
+        //Send response to HipChat Room
+        val myRequest = sendAsyncResponse(urlStr, hipchatMsg, authToken) <<
+          """{"color": "green", "message": """ + "\"" + "@" + hipchatMsg.item.message.from.mentionName + " " +
+            result.replace("\n", "\\n") + "\"" + """, "notify": false, "message_format": "text"}"""
+        dispatch.Http(myRequest)
+      }
+      case Failure(ex: Exception) => {
+        val myRequest = sendAsyncResponse(urlStr, hipchatMsg, authToken) <<
+          s"""{"color": "red", "message": "Sorry your scala code has failed with ${ex.getMessage}
+             |", "notify": false, "message_format": "text"}""".stripMargin
+        dispatch.Http(myRequest)
+      }
+
+    }
+
+  }
+
+  def sendAsyncResponse(urlStr: String, hipchatMsg: HipChatMessage, authToken: String): Req = {
+    val roomId = hipchatMsg.item.room.id
+    val updatedUrl = urlStr.replace("roomId", roomId.toString).concat(authToken)
+    val request = dispatch.url(updatedUrl).setContentType("application/json", "UTF-8").POST
+    request
   }
 }
+
