24a4466cce2aff2689848aa0d2b3df8af8c8bd92
howardlunatech <howard@lunatech.com>
2015-10-15 17:57:49 +0200

    Remove unnecessary text output 1. Remove first 4 lines of welcome message, and the last 2 lines of auto-generated ":quit".

--- src/main/scala/com/lunatech/lunabot/REPLproc.scala
+++ src/main/scala/com/lunatech/lunabot/REPLproc.scala
@@ -17,8 +17,8 @@ object REPLproc {
   * @param code String, Scala code to be executed.
   * @return String, scala REPL response, STDERR after STDOUT.
   * @example val responseLinesOfString = REPLproc.run(scalaCodeString)
-  * @todo Remove the 2,3 lines of welcome message, and the last line of auto-generated ":quit".
-  * @todo TODO: seperate STDOUT and STDERR to allow different coloring.
+  * @todo Make REPL persistent process, using :reset to clean up before each session.
+  * @todo Seperate STDOUT and STDERR to allow different coloring.
   */
 
   def run (code: String): String = {
@@ -26,9 +26,9 @@ object REPLproc {
     val process = Process (cmd)
     val io = new ProcessIO (
       in  => {new PrintStream(in).println(code); in.close},
-      out => {respOut = Source.fromInputStream(out).getLines.mkString("\n"); out.close}, // TODO: Remove line 2,3 of welcome message, and the last line of auto-generated ":quit".
+      out => {respOut = Source.fromInputStream(out).getLines.toArray.drop(4).dropRight(2).mkString("\n"); out.close}, // Remove first 4 lines of welcome message, and the last 2 lines of auto-generated ":quit".
       err => {respErr = Source.fromInputStream(err).getLines.mkString("\n"); err.close})
-    process.run(io).exitValue // Wait until the process exit, otherwise respOut and respErr won't get output yet.
+    process.run(io).exitValue // Wait until the process exits, otherwise respOut and respErr won't get output yet.
     respOut + respErr // TODO: seperate STDOUT and STDERR to allow different coloring.
   }
 }
