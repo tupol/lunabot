45d338a7515c7457ec0891d990a9632d6cd167fd
olivertupran <oliver.tupran@lunatech.com>
2015-10-21 14:00:15 +0200

    Work in progress. Major refactoring!!!!

---
+++ src/main/scala/com/lunatech/lunabot/HipChatResponder.scala
@@ -0,0 +1,8 @@
+package com.lunatech.lunabot
+
+/**
+ * Created by olivertupran on 21/10/15.
+ */
+trait HipChatResponder {
+
+}
---
+++ src/main/scala/com/lunatech/lunabot/package.scala
@@ -0,0 +1,8 @@
+package com.lunatech
+
+/**
+ * Created by olivertupran on 21/10/15.
+ */
+package object lunabot {
+
+}
